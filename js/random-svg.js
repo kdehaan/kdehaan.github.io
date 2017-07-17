45a189852b0e703ef144c6c466824f0b78cf2191
Kevin de Haan <kdehaan@novosbed.com>
2017-07-17 14:15:12 -0600

    randomized svg (partially) and reconfigured background image

---
+++ js/random-svg.js
@@ -0,0 +1,25 @@
+
+
+function randomizeSvg( svgname, classname ) {
+  var svgdoc;
+  var shades = [
+	'#d3d3d3',
+	'#bdbdbd',
+	'#a8a8a8',
+	'#939393',
+	'#acacac',
+	'#b9b9b9',
+	'#c6c6c6',
+	'#696969',
+	'#7e7e7e'
+	];
+  svgname.addEventListener("load", function() {
+    svgdoc = svgname.contentDocument;
+    console.log(svgdoc);
+    splashFeathers = svgdoc.getElementsByClassName(classname);
+    for (var i=0; i<splashFeathers.length; i++) {
+      randomShade = shades[Math.floor(Math.random()*shades.length)];
+      splashFeathers[i].setAttribute("fill", randomShade);
+    }
+  }, false);
+}
