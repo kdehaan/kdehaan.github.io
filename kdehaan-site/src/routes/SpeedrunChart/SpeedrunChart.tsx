0a864152e150c06e4339b256a8326b366bcf22fe
Kevin de Haan <kevin@dehaan.ca>
2022-09-26 01:14:59 -0700

    some staging progress

--- kdehaan-site/src/routes/SpeedrunChart/SpeedrunChart.tsx
+++ kdehaan-site/src/routes/SpeedrunChart/SpeedrunChart.tsx
@@ -5,7 +5,10 @@ import * as d3 from 'd3'
 
 const LINE_WIDTH = 0.6
 const baseDataSet = generateDataset().sort()
+console.log(baseDataSet)
 const colorScale = d3.scaleOrdinal().domain(baseDataSet.length.toString()).range(d3.schemeDark2)
+
+
 
 const SpeedrunChart = () => {
 
@@ -17,6 +20,16 @@ const SpeedrunChart = () => {
     // generateDataset().sort()
     baseDataSet.slice(0, numPoints)
   )
+
+  const [stage, setStage] = useState(
+    2
+  )
+ 
+  // const stepGraph = () => {
+  //   setStage((stage + 1)  % 3)
+  //   setNumpoints(Math.min(numPoints + (stage === 2 ? 1 : 0), baseDataSet.length-1))
+  //   setDataset(baseDataSet.slice(0, numPoints))
+  // }
   
 
   const ref = useRef()
@@ -47,9 +60,23 @@ const SpeedrunChart = () => {
     //   .domain([0, 100])
     //   .domain([10, 100])
 
+    svgElement.on("click", () => {
+      setStage((stage + 1)  % 3)
+      setNumpoints(Math.min(numPoints + (stage === 2 ? 1 : 0), baseDataSet.length-1))
+      setDataset(baseDataSet.slice(0, numPoints))
+    })
+
+    console.log(dataset, stage, numPoints)
+    let h_dataset = dataset.slice(0, dataset.length)
+    console.log(h_dataset.length)
+    let v_dataset = dataset.slice(0, dataset.length - (stage === 1 ? 1 : 0))
+    console.log(v_dataset.length)
+    let p_dataset = dataset.slice(0, dataset.length - (stage > 0 ? 1 : 0))
+    console.log(p_dataset.length)
+
     vLinesElement
     .selectAll("line")
-    .data(dataset)
+    .data(v_dataset)
       .join(
         (enter: any) =>
           enter
@@ -61,10 +88,13 @@ const SpeedrunChart = () => {
             .attr("y1", (d: number[], i: number) => dataset[Math.max(0, i-1)][1]) 
             .attr("x2", (d: number[], i: number)=> d[0])
             .attr("y2", (d: number[], i: number) => dataset[Math.max(0, i-1)][1]) 
-            .call((enter: any) =>
-              enter
-                .transition()
-                .attr("y2", (d: number[], i: number) => d[1])
+            .call((enter: any) => {
+                enter
+                  .transition()
+                  .attr("y2", (d: number[], i: number) => d[1])
+                  // .transition()
+                  // .style("stroke-width", LINE_WIDTH*2)
+              }
             ),
           (update: any) =>
             update.call((update: any) =>
@@ -84,7 +114,7 @@ const SpeedrunChart = () => {
          
           
     hLinesElement.selectAll("line")
-    .data(dataset)
+    .data(h_dataset)
     .join(
       (enter: any) =>
         enter
@@ -96,11 +126,12 @@ const SpeedrunChart = () => {
           .attr("y1", (d: number[], i: number) => dataset[Math.max(0, i-1)][1])
           .attr("x2", (d: number[], i: number)=> dataset[Math.max(0, i-1)][0])
           .attr("y2", (d: number[], i: number) => dataset[Math.max(0, i-1)][1])
-          .call((enter: any) =>
-            enter
+          .call((enter: any) => {
+              enter
               .transition()
               .attr("x2", (d: number[], i: number)=> d[0])
               // .attr("y2", (d: number[], i: number) => d[1])
+            }
           ),
         (update: any) =>
           update.call((update: any) =>
@@ -125,23 +156,45 @@ const SpeedrunChart = () => {
       //   .attr("y2", (d: number[], i: number) => d[1])
 
     dataPointElement.selectAll("circle")
-    .data(dataset)
-      .join("circle")
-        .attr("fill", (d: number[], i: number) => colorScale(i.toString()) as string)
-        .attr("cx", (d: number[])=> d[0])
-        .attr("cy", (d: number[]) => d[1])
-        .attr("r",  1)
+    .data(p_dataset)
+    .join(
+      (enter: any) =>
+        enter
+          .append("circle")
+          .attr("fill", (d: number[], i: number) => colorScale(i.toString()) as string)
+          .attr("cx", (d: number[])=> d[0])
+          .attr("cy", (d: number[]) => d[1])
+          .attr("r",  0)
+          .call((enter: any) => {
+              enter
+              .transition()
+              .attr("r",  1)
+            }
+          ),
+        (update: any) =>
+          update.call((update: any) =>
+            update
+              .transition()
+          ),
+        (exit: any) =>
+          exit.call((exit: any) =>
+            exit
+              .transition()
+              .remove()
+          ),
+    )
 
     // const axisGenerator = d3.axisBottom(xScale)
     // svgElement.append("g")
     //   .call(axisGenerator)
-  }, [dataset])
+  }, [dataset, numPoints, stage])
 
-  useInterval(() => {
-    // const newDataset = generateDataset().sort()
-    setNumpoints(Math.min(numPoints + 1, baseDataSet.length-1))
-    setDataset(baseDataSet.slice(0, numPoints))
-  }, 1000)
+  // useInterval(() => {
+  //   stepGraph()
+  // }, 2000)
+
+
+ 
 
   return (
     <svg
